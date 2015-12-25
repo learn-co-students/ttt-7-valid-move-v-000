d4d6dc3c8d4e01b7c2dbcff9a26fd685dff7b83d
Chris Aporta <christopher.aporta@gmail.com>
2015-12-25 16:20:18 -0500

    restructure lab

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,12 +3,12 @@ require 'pry'
 
 def valid_move?(board, position)
   indexed_position = position.to_i - 1
-  indexed_position.between?(0,8) && position_valid?(board, indexed_position)
+  indexed_position.between?(0,8) && !position_taken?(board, indexed_position)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_valid?(board, position)
-  !(board[position] == 'X' || board[position] == 'O')
+def position_taken?(board, position)
+  !!(board[position] == 'X' || board[position] == 'O')
 end
 
