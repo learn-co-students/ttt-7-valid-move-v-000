4a2db0910109d41124ffd7f93d289811aab239e1
Hugo M <h.murillo21@yahoo.com>
2016-03-15 22:24:19 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def valid_move?(board, position)
        false
      end
    end
- end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
  def position_taken?(board, location)
    board[location] == " "
