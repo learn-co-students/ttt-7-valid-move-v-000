0f85a1505995a6205d29664f1ef9acb9e9881500
bhabig <rfhab2@gmail.com>
2016-04-06 16:29:38 +0000

    nitrous-backup at Wed Apr  6 16:29:37 UTC 2016

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,16 @@
-def valid_move?(board, position)
+#def valid_move?(board, position)
+#  if position.to_i.between?(1, 9) && position_taken?(board, position) == false
+ #   true
+  #else
+   # false
+ # end
+#end
 
-  if position.to_i.between?(1, 9) && position_taken?(board, position) == false
-    true
-  else
-    false
-  end
-end
-
-def position_taken?(board, position)
-  if board[position.to_i-1] == " " || board[position.to_i-1] == "" || board[position.to_i-1] == nil
-    false
-  else board[position.to_i-1] == "X" || board[position.to_i-1] == "O"
-    true
-  end
-end
+#def position_taken?(board, position)
+  #if board[position.to_i-1] == " " || board[position.to_i-1] == "" || board[position.to_i-1] == nil
+    #false
+  #else board[position.to_i-1] == "X" || board[position.to_i-1] == "O"
+   # true
+  #end
+#end
 
