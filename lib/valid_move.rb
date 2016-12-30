62916f82c0562a9524fa0dbd57215ce85da718ce
David Wistocki <david@davidwistocki.com>
2016-12-30 21:48:29 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
-# code your #valid_move? method here
+def valid_move?(board, index)
+  index = index.to_i
+  if index.between?(0,8) == false
+    return false
+  elsif index.between?(0,8) == true && position_taken?(board, index) == true
+    return false
+  elsif index.between?(0,8) == true && position_taken?(board, index) == false
+    return true
+  else return false
+  end
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+  end
+end
