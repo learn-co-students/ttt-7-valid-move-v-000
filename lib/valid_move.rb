ff9af8e934918dcff51e35014dc2a25c099f8c0e
Tavish Harrison <creationmargin26@gmail.com>
2017-01-04 02:25:55 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
-  if index.between?(0, 8)
+  if index.between?(0, 8) && !position_taken?(board, index)
     true
-  else
+  elsif board[index] == "X" || "O"
     false
   end
 end
