32601f5cdb91dfd5521c8e577d8dac1da18eff84
James Borocz <thesilentc@me.com>
2017-01-25 18:28:03 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,8 +6,16 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def position_taken?(board, index)
-  (board[index] ==  " " || board[index] == " " || board[index] == nil) ?
-   false : true
+#def position_taken?(board, index)
+#  (board[index] ==  " " || board[index] == " " || board[index] == nil) ?
+#   false : true
 
+#end
+
+def position_taken?(board, index)
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  false
+  else board[index] == "X" || board[index] == "O"
+  true
+  end
 end
