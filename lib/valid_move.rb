a9b97654d31e34c41c762772568abac518fd7ee5
Emily Sugano <emily.sugano@gmail.com>
2017-03-02 22:00:37 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,23 +1,11 @@
 def position_taken?(board, index)
-  if board[index] == " "
-    return false
-  elsif board[index] == ""
-    return false
-  elsif board[index] == nil
-    return false
-  else
-    return true
-  end
+  board[index] != "" && (board[index] != nil) && (board[index] != " ")
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == true
-    return nil
-  elsif (index).between?(0,8) == false
+  if position_taken?(board, index) == true || ((index).between?(0,8) == false)
     return false
   elsif position_taken?(board, index) == false && ((index).between?(0,8) == true)
     return true
-  else
-    return false 
   end
 end
