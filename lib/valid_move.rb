80f68fa30f571ec16f3644cdf096c4b3853870a1
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-03-06 19:17:11 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,6 @@ end
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index.between?(0,8) && !(position_taken?(board, index))
-    return true
   else
     return false
   end
