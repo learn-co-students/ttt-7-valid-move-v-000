f97e4031145f834a5d327913a0f6934a071bd741
Noah Summers <summers.noah@gmail.com>
2017-04-19 21:50:15 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
 # code your #valid_move? method here
 
+def valid_move?(brd, ind)
+  if ind.between?(0,8)
+    return true unless position_taken?(brd, ind)
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(brd, ind)
+  case brd[ind]
+  when "X", "O"
+    return true
+  when nil, " ", ""
+    return false
+  end
+end
