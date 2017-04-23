216273891ba51340ad14eee100ea9fa7b053c6e0
Louise Valmoria <lvalmoria@gmail.com>
2017-04-23 11:21:55 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,24 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+
+if index > 8 #returns nil or false for a position that is not on the board, because any index > 8 is not valid
+  false
+elsif position_taken?(board,index) == false #returns true for a valid position on an empty board
+  true
+else
+  false # returns nil or false for an occupied position
+end
+
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+
+if board[index] == "X" || board[index] == "O"
+  true
+elsif board[index] == " " || board[index] == "" || board[index] = "nil"
+  false
+end
+
+end
