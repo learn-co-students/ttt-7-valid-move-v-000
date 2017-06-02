dca56e6347f089ff731db60b4bc16c037d713a68
Po-Yi Chi <wcc0529@gmail.com>
2017-06-02 19:48:01 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
 # code your #valid_move? method here
+def valid_move?(array,index)
+
+  if array[index] == " "
+    return true;
+  else
+    return false;
+  end
+end
+
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
