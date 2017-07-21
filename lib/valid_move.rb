71ca1ad05a7ac344a8818783a28bfc532ce98d80
Camille Neuner <neuner.camille@gmail.com>
2017-07-21 06:35:26 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,37 @@
+#def valid_move?(board, index)
+#  if position_taken?(board, index) == true
+#    return false
+#  elsif index > 8
+#    return false
+#  else
+#    return true
+#  end
+#end
+
 def valid_move?(board, index)
-  if position_taken?(board, index) == true
-    return false
-  elsif index > 8
-    return false
+  if position_taken?(board, index) || index > 8
+    false
   else
-    return true
+    true
   end
 end
 
 
+#def position_taken?(board, index)
+#  if board[index] == " "
+#    false
+#  elsif board[index] == ""
+#    false
+#  elsif board[index] == nil
+#    false
+#  else
+#    true
+#  end
+#end
+
+
 def position_taken?(board, index)
-  if board[index] == " "
-    false
-  elsif board[index] == ""
-    false
-  elsif board[index] == nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
   else
     true
