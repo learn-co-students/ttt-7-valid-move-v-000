7db76cf67f660d518cb0870a15e41311b3cb4a4d
Juliet <jktallmer2@gmail.com>
2017-08-25 01:55:24 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,20 @@
-def valid_move?(board,index)
-  if position_taken?(board,index) == false && index.between?(0,8)
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
     true
   else
     false
   end
 end
 
-def position_taken?(board,index)
+
+
+def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  else
+  else board[index] == "X" || board[index] = "0"
     true
   end
 end
+
+#access element of board through index number
+#if position is empty then it's not taken, so returns false
