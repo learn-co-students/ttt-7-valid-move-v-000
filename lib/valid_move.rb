6a1a2b931d7452bfde920c467f7235619e10cfce
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-09-12 20:49:50 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
 # code your #valid_move? method here
-
+def valid_move?(board,index)
+  # you want to make sure you have a valid index that is between 0 and 8
+  if !position_taken?(board,index)
+    true
+ end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+  # we have a board which is an array
+  # we have an index which leads to a spot in the board, integer
+  board[index] == "X" || board[index] == "O"
+end
