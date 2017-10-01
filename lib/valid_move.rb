bad61dd2db80ffd80f2c3c3c0b6dc85c6f1a7793
Joshua Needham <joshuabneedham@gmail.com>
2017-10-01 23:59:37 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,4 +7,17 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
+  #Check for an empty board is equal to " "
+  if board[index] == " "
+    return false
+  end
+  if board[index] == ""
+    return false
+  end
+  if board[index] == nil
+    return false
+  end
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
 end
