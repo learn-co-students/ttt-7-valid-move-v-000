ae85bd03d2a612faacf1861eed780d55dbc4ce74
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-06 19:07:06 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  # is the move between index 0 and 8, 1 - 9 on the player side (i.e. on the game board)
+  # silent "is" before your range expression
+  (0..8).include?(index) && !position_taken?(board,index)
+  # is the move in a space that has an X or O
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
