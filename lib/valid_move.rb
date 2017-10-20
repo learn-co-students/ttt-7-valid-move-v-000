b9751f55d42de1693b9e90b504623f02be70b7c7
Sara Schwarz <saramschwarz@gmail.com>
2017-10-20 01:24:08 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,18 @@
-# code your #valid_move? method here
+def valid_move?(board, index)
+if position_taken?(board, index) && index.between?(0, 8)
+  false
+else true
+end
+end
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+taken = nil
+if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+taken = false
+else
+taken = true
+end
+end
+
+#The move is valid when the user picks 0:8, the space is empty and " " or " "
