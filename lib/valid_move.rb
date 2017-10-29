fdba02c3195614504c94ae4ea888df528c3b2b08
github username <github email address>
2017-10-29 00:31:20 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
 # code your #valid_move? method here
 
+def valid_move?(board,index)
+   if position_taken?(board,index)
+     false
+   elsif !board[index]
+     false
+   else 
+     true
+   end
+
+
+end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board,index)
+  if board[index] == nil
+    false
+  elsif board[index].strip.empty?
+    false
+  else
+    true
+  end
+end
