553f062f5f66a7ad4dc6fdbdc6390ddff49b2edf
Chris Kihlstrom <ckihlst@gmail.com>
2017-10-29 19:51:13 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,12 @@
-def valid_move(board,index)
-  if def position_taken?(board,index)
+def position_taken?(board,index)
     board[index] != " " && board[index] != "" && board[index] != nil
-  else
+end
+
+def valid_move?(board,index)
+  if position_taken?(board,index)
+     false
+  elsif index >= 0 && index < 9
+    true
+  else false
   end
 end
