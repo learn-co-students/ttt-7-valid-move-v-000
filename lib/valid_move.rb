246377e446347718e917946e8de2fd60c1f32526
James Dolan <jwdolan820@gmail.com>
2017-11-13 03:37:19 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board, index)
   if index.between?(1,9)
-    if !position_taken?(board, index)
+    if !position_taken?(board, index) #=>if position is not taken, returns true
       true
     end
   end
@@ -8,6 +8,10 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
+
 def position_taken?(board, index)
-  board[index] != " "
+  board[index] != " "     #=>position is taken
 end
+
+
+#Line 8: returns true if index is between 0-8 and position isn't taken
