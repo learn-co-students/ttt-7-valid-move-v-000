6edb1e862fa26a232654e4e84d871cf13a45c9e4
Kristen Leach <kristenleach24@gmail.com>
2017-11-26 20:46:12 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,11 @@
 def valid_move?(board, index)
-  if position_taken? || !index.between?(0-8)
-    return false
-  elsif !position_taken? && index.between?(0-8)
-    return true
-  end
+  index.between?(0, 8) && !position_taken?(board, index)
 end  
-
 
 def position_taken?(board, index)
   if  (board[index] == "") || (board[index] == " ") || (board[index] == nil)
     return false
-  elsif (board[index] != "") || (board[index] != " ")
+  elsif (board[index] != "") || (board[index] != " ") ||(board[index] != nil)
     return true
   end
 end 
