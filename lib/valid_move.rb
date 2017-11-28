5edbafeedae1ef3f7547b95b4916226adea43d5f
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2017-11-28 14:43:34 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,14 +1,18 @@
+# Reminder: conversion to integer will be done seperately
+
+# 1. -COMPLETE- index must be a valid index 0-8
+# 2. -COMPLETE- index must be empty " " or ""
+# (remember as long as CONDITION is true boolean return value is true no need to explicitly "return true")
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  index.between?(0, 8) && !made_move?(board, index)
 end
 
-
-def position_taken?(board, index)
-   if  board[index] == "" || board[index] == " " || board[index] == nil
-     return false
-   else
-     return true
-   end
+# 1. Did user input anything?  -COMPLETE
+# *user input must not be empty string " " or "" (return true)
+def made_move?(board, index)
+  if board[index] == " " || board[index] == ""
+    return false #index is empty
+  else
+    return true #index is taken
+  end
 end
-
-this = "3"
