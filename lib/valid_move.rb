b9b9b2fe40f7348070b8464c728bd9663159a963
Kayla Cross <kac288@georgetown.edu>
2018-01-29 13:15:15 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
 # code your #valid_move? method here
 
-
+def valid_move?(board, move)
+  if position_taken?(board, move)
+    puts "There's already something there. Please choose a free space."
+    return false
+  elsif move >= 0 && move <= (board.length-1)
+    return true
+  end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, move)
+  if board[move] == "" || board[move] == " " || board[move] == nil
+    return false
+  elsif board[move] == "X" || board[move] == "O"
+    return true
+  end
+end
