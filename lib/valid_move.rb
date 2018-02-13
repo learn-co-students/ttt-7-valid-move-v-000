16c315e059936cffc6d418c0305cf0c495a3b84c
Lluvia Frias <lluvi.frias@gmail.com>
2018-02-13 04:37:31 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,8 @@ def valid_move?(board, index)
 
   if index.between?(0,8) && !position_taken?(board, index)
     true
-  else index.between?(0,8) && position_taken?(board, index)
-    nil
+  else
+    false
   end
 end
 
