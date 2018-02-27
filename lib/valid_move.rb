1c78dc24cf28849cb4fd95de730e4db1442e02a6
Ethel C <cofresi.ethel@gmail.com>
2018-02-27 18:29:32 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
-# code your #valid_move? method here
+#eCofresi
+# Check if move is valid - within board and empty and
+# Positon not taken - call position_taken? helper_method
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+    if board[index] == "X" || board[index] ==  "O"
+      true
+    elsif board[index] == " " || board[index] == "" || board[index] == nil
+      false
+    end
+end
+  
+def valid_move?(board, index)
+  puts position_taken?(board, index)
+  if index == nil || position_taken?(board, index) || not(index.between?(0,8))
+        false
+  else
+        true
+  end
+end
