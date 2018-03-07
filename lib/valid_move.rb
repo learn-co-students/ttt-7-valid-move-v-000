b72fc2b0adb0841bf92a8f4e72f8a50fc781eb83
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-03-07 14:46:05 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == true
     return false
-  elsif board[index] == nil
+  elsif board[index] == nil || false
     return false
   else position_taken?(board, index) == false
     return true
