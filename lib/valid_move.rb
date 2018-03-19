4cc5c19d85b0976dce146d2102882fcce98de307
github username <github email address>
2018-03-18 20:30:43 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,24 @@
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
 # code your #valid_move? method here
+def valid_move?(board,index)
+  if position_taken?(board, index) == false && index.between?(0,8) == true
+    true
+  else
+    false || nil
+  end
+end
+
+#returns true for a valid position on an empty board (is between 1- 8 index?) (FAILED - 1)
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+ #returns true for a valid position on a non-empty board (passed)(both 1 -8 and not occupied?)
+ #returns nil or false for an occupied position (passed)
+ #returns nil or false for a position that is not on the board(passed)
