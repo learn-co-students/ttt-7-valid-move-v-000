eaba6b184cd4b5fd0a267f2c9566c7c33cda436b
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-04-11 23:30:23 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,7 @@
 def valid_move?(board, index)
-  if index > 0 && index < 9 && position_taken?(board, index) == true
-    true
-  else
-    false
-  end
+   index.between?(0,8) && position_taken?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    true
-  elsif board[index] == "X" || board[index] == "O"
-    false
-  end
+  board[index] == " " || board[index] == "" || board[index] == nil
 end
