5d34577bfc6fe5fa163b1396c716d1819a3771c1
Shirley Collins <shirley.ednave@gmail.com>
2018-05-08 06:34:26 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  #if the position is 9 or higher then it's outside the tictactoe board
+  if index > 8
+    return false
+  #if the position is empty, then it's a valid move
+  elsif !position_taken?(board, index)
+    return true
+  end
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == 'X' || board[index] == 'O'
+    return true
+  end
+end
