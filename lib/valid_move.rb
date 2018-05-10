f53749b680ede095e24b0a87917b034ddaa89344
JohnMark <johnmarkludwick@gmail.com>
2018-05-10 13:06:08 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,16 @@
-# code your #valid_move? method here
-
+def valid_move?(board, index)
+  #if the position is not taken then it's valid & the move must be between 0 and 8.
+  if !position_taken?(board, index) && index.between?(0,8) == true
+  true
+  end  
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+#if position is filled with X or O the position is taken.
+def position_taken?(board, position)
+  if board[position] == "X" || board[position] == "O"
+    true
+    else
+    false
+    end
+end
