671af45fd4d74b1bf1c74c4c4d1575a7c20d6567
Omar Khaliq <omargraphene@gmail.com>
2018-07-10 20:27:04 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if board[index] > 9 || < 1
+  if board[index] == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 
+    return true
+  elsif board[index] =  
+    return false
   end
 end
 
