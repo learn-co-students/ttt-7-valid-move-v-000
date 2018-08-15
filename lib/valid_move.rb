3450f7c0b536bf026f6b5875718693776e3c8c87
Ashley Wong <ashwong04@gmail.com>
2018-08-15 19:22:42 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move?(board, index)
     
-    if (position_taken?(board, index) == false) && (board[index].between (0, 8) == true)
+    if (position_taken?(board, index) == false && index.between?(0, 8) == true)
       return true
   
     elsif position_taken?(board, index) == true
