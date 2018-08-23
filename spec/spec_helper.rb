f817b286cc8a988417c43c2f3c8bf638be31927c
Kristen Robertson <kristen77@gmail.com>
2018-08-23 03:22:03 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,19 @@
 RSpec.configure do |config|
   config.order = :default
 end
+board = [" ", " ", " ", "X ", " ", " ", " ", " ", " "]
+def valid_move(board, index)
+  
+if index.between(1,8) == " "
+  return true 
+
+elsif 
+ index.between(1,8) == "X"
+  return true
+  
+elsif 
+  index.between(1,8) == " "
+  return false 
+end
+
+end
