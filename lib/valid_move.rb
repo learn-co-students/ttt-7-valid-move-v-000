168462a631ecbeea2d6c64ae70f8cc3096c68d36
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-08 14:54:14 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,3 +9,17 @@ def position_taken?(board, index)
     TRUE
   end
 end
+
+#def valid_move?(board, index)
+  # if index.between?(1,9)
+  #   if !position_taken?(board, index)
+  #     true
+  #   end
+  # end
+
+  #index.between?(0,8) && !position_taken?(board, index)
+#end
+
+#def position_taken?(board, index)
+  #board[index] != " "
+#end
