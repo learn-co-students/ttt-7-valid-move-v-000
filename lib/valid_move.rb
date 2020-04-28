6d0c08e840fc9bf3342a075944df0b8ca20e360d
Joann Pan <joannlpan@gmail.com>
2020-04-28 23:30:27 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+
+#true if the move is valid
+#a. Present on the game board
+#b. Not already filled with a token.
+
+#false or nil if not
+
+
+
+
+
+
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  board[index] != " "
+end
