3471fa30470e311fefc9fa2f4a17375f2197eefc
Ashleigh Petty <petty.ashleigh@yahoo.com>
2020-05-13 17:54:29 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,49 @@
 # code your #valid_move? method here
-
-
+# #def valid_move?(board, index)
+#   if position_taken? == false 
+#     return true
+#   else
+#     return false 
+#   end 
+# end
+    
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
+end
+
+
+
+
+
+
+# def position_taken?(board, index)
+#   if board[index] == " " || board[index] == "" || board[index] == NIL
+#     return false
+#   else  
+#     return true 
+#   end
+# end
+    
