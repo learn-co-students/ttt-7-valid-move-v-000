d01d5152b5c9763ca3caec328039def49a020d9e
Joan' Araujo <wisdombthegame@gmail.com>
2020-06-26 18:15:48 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,19 @@
-def valid_move?(board, index)
-if index.between?(1,9)
-if !position_taken?(board, index)
-true
-end
-end
-
-index.between?(0,8) && !position_taken?(board, index)
-end
-
 def position_taken?(board, index)
-  board[index] != " "
+(board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
 end
+
+  
+#1. #valid_move? method that accepts a board and an index to check?(position_taken?) AND returns TRUE if the move is valid and FALSE or nil if not. A valid move means that the submitted position is:
+
+#Present on the game board.
+#Not already filled with a token. 
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else 
+    return false
+  end
+end
+   
+   
