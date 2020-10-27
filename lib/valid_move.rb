b47885e5f13423219fa52dd1c8c4266d70056bc7
danpaslawsky <danpaslawsky@gmail.com>
2020-10-27 16:35:15 -0700

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,19 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-    #check to see IF user picked position between 0-8 (use #.between? method) then execute ?
-    if position_taken?(board, index)
-        false
-    else board[index].to_i.between?(0, 8)
-        binding.pry 
-        true
-    end
+    index.between?(0, 8) && !position_taken?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-    if board[index] == " " || board[index] == "" || board[index] == nil
-        false
-    elsif board[index] == "X" || board[index] == "O"
-        true
-    end
+    !board[index] == " " || !board[index] == "" || !board[index] == nil || board[index] == "X" || board[index] == "O"
 end
